f83b1697dc3e16b73dcdec04945b591c85669111
Bryan JJ Buckley <jjbuckley@gmail.com>
2013-07-28 14:58:19 +0200

    Working custache parser
    So far, handles sections, inverted sections, basic content, and static content.
    TODO:
    [ ] delimiter changes
    [ ] comments

--- custache.c
+++ custache.c
@@ -3,137 +3,144 @@
 #include <string.h>
 #include <assert.h>
 
-#define LENGTH(x) (sizeof(x) / sizeof(*x))
-
+/**
+ * Types of template nodes
+ */
 typedef enum {
-  STATIC = 0,
+  STATIC,
   BASIC,
   SECTION,
-  INVERT
-} template_type_t;
+  INVERT,
+  CLOSING
+} token_type_t;
 
+/**
+ * A Template node
+ */
 typedef struct Template {
-  template_type_t type;
+  unsigned char type;
   struct Template *next;
   struct Template *child;
   char *content;
 } template_t;
 
-typedef struct Context {
-} context_t;
+static char *parse(template_t *, char * , char * , char * );
+static char *parse_static(template_t *, char *, char * , char * );
+static char *parse_mustache(template_t *, char * , char * , char * );
 
-static template_t *compile(char *);
-static void parse(template_t *, char *, char *, char *);
-static void mustache(template_t *, char *, char *, char *);
+static void _dump(template_t *t, unsigned int indent) {
+  int i; for (i = indent; i; i--) printf(" ");
+  printf("[%p %u %p %p '%s']\n", t, t->type, t->child, t-> next, t->content);
+  if (t->child) _dump(t->child, indent + 2);
+  if (t->next)  _dump(t->next, indent);
+}
 
-static void debug_template(template_t *, unsigned int);
+static void dump(template_t *t) {
+  _dump(t, 0);
+}
 
-char *custache(char *template, void *context) {
-  template_t *t = compile(template);
-  char *result;
-  asprintf(&result, "Template at %p\n", t);
+/**
+ * Frees the template, by freeing all children, then all following nodes, then
+ * its content, and finally the template itself.
+ */
+static void free_template(template_t *t) {
+  if (t->child)   free_template(t->child);
+  if (t->next)    free_template(t->next);
+  if (t->content) free(t->content);
   free(t);
-  return result;
 }
 
-static template_t *compile(char *src) {
-  template_t *t = malloc(sizeof *t);
-  parse(t, src, "{{", "}}");
-  return t;
+/**
+ * Fills the template at t from the string at s. otag and ctag are used as
+ * tag delimiters. Returns NULL if s has been parsed completely, otherwise
+ * returns a pointer to the next unprocessed character in s.
+ *
+ * t must contain enough space for the template.
+ */
+static char *parse(template_t *t, char * s, char * otag, char * ctag) {
+  char *p = strstr(s, otag);
+
+  if (p == s)
+    p = parse_mustache(t, p + strlen(otag), otag, ctag);
+  else
+    p = parse_static(t, s, otag, ctag);
+
+  return p;
 }
 
-static void parse(template_t *t, char *src, char *otag, char *ctag) {
-  size_t size = strlen(otag);
-  char *ptr = src;
+/**
+ * Fills the template t with static content from the string s. Returns a pointer
+ * to the next character in s.
+ */
+static char *parse_static(template_t *t, char * s, char * otag, char *ctag) {
+  char *p = strstr(s, otag);
+  size_t n = p ? p - s : strlen(s);
 
-  printf("PARSE   : ");
-  debug_template(t, 0);
+  t->type = STATIC;
+  t->content = strndup(s, n);
 
-  if (!*ptr) {
-    return;
+  if (p) {
+    t->next = calloc(1, sizeof *t->next);
+    p = parse(t->next, p, otag, ctag);
   }
 
-  ptr = strstr(src, otag);
-  if (!ptr) {
-    /* Blow up if we're expecting more content */
-    assert(!t->type);
-  }
-  else if (ptr == src) {
-    /* No static content - {{mustache}} */
-    bzero(ptr, size); /* Terminate the string, for the preceding templates */
-    ptr += size; /* Go to the start of the {{mustache}} */
-    mustache(t, ptr, otag, ctag);
-  }
-  else {
-    /* Static content - build the template */
-    t->type = STATIC; /* It's boring static content... */
-    t->next = malloc(sizeof *t); /* ... but possibly some additional content. */
-    t->content = src; /* Remember this string... */
-    parse(t->next, ptr, otag, ctag);
-  }
-
-  printf("PARSEEND: ");
-  debug_template(t, 0);
-
+  return p;
 }
 
-static void mustache(template_t *t, char *src, char *otag, char *ctag) {
-  size_t size = strlen(ctag);
-  char *ptr = src;
+/**
+ * Fills the template t with mustache content. Returns a pointer to the next
+ * character in s.
+ */
+static char *parse_mustache(template_t *t, char * s, char * otag, char * ctag) {
 
-  printf("MUSTACHE: ");
-  debug_template(t, 0);
+  char *p = strstr(s, ctag);
+  assert(p);
 
-  switch (*ptr++) {
-    case '#': /* section */
-      t->type = SECTION;
-      t->child = malloc(sizeof *(t->child));
-      break;
-    case '^': /* inverted section */
-      t->type = INVERT;
-      t->child = malloc(sizeof *t);
-      break;
-    case '/': /* end of section */
-      assert(t->type == SECTION);
-      assert(!strcmp(t->content, ptr + 1));
-      break;
-    case '=': /* delimiters */
-      break;
-    case '>': /* partial */
-      break;
-    default: /* basic */
-      ptr--;
-      t->type = BASIC;
-      t->next = malloc(sizeof *(t->next));
+  switch (*s) {
+  case '#':
+  case '^':
+    t->type = (*s == '#' ? SECTION : INVERT);
+    t->content = strndup(s + 1, p - s - 1);
+    p += strlen(ctag);
+    t->child = calloc(1, sizeof *t->child);
+    p = parse(t->child, p, otag, ctag);
+    break;
+  case '/':
+    t->type = CLOSING;
+    t->content = strndup(s + 1, p - s - 1);
+    p += strlen(ctag);
+    return p;
+  default:
+    t->type = BASIC;
+    t->content = strndup(s, p - s);
+    p += strlen(ctag);
+    break;
   }
-  t->content = ptr;         /* remember the tag name */
-  ptr = strstr(ptr, ctag);  /* go to the start of the closing tag */
-  assert(ptr);              /* make sure there IS a closing tag! */
-  bzero(ptr, size); /* terminate the string */
-  ptr += size;      /* go to the end of the closing tag */
 
+  if (p) {
+    t->next = calloc(1, sizeof *t->next);
+    p = parse(t->next, p, otag, ctag);
+  }
+
+  return p;
 }
 
-static void debug_template(template_t *t, unsigned int indent) {
-  int i;
-  for (i = indent; i; i--) printf(" "); 
-  printf("[%p TYPE %o CHILD %p NEXT %p CONTENT '%s']", t, t->type, t->child, t-> next, t->content);
-  if (t->child) debug_template(t->child, indent + 2);
-  if (t->next) debug_template(t->next, indent);
-  printf("\n");
+/**
+ * Makes a template from the string s, and uses it to render content with the
+ * view context. Returns the rendered result, which should then be freed.
+ */
+char *custache(char *s, void *context) {
+  template_t *t = calloc(1, sizeof *t);
+  parse(t, s, "{{", "}}");
+  dump(t);
+  free_template(t);
+  return "";
 }
 
 int main(int argc, char **argv) {
   char *result;
 
-  printf("Starting...\n");
-
   result = custache(argv[1], argv[2]);
-
-  printf("%s\n", result);
-
-  free(result);
 
   return 0;
 }
-
